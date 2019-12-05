a94c3fde7e875c7c4a71ba1439d5ee3e7a62782b
Brian Pompey <brianthescholar@gmail.com>
2019-12-05 23:36:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,6 +6,10 @@ class App < Sinatra::Base
     erb :index
   end
 
+  get '/new' do
+    erb :create_puppy
+  end
+
   post '/puppy' do
     @puppy = Puppy.new(params[:puppy])
     erb :display_puppy
